0efccfe6adc6c720ed50003f7fd1d9b537acf0e0
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2023-10-09 13:53:46 -0500

    Seaerch by City

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 /* eslint-disable react/function-component-definition */
 import CityPicker from '@/components/CityPicker';
-import { Card, Divider, Subtitle, Text } from '@tremor/react';
+import { Card, Divider, Subtitle, Text, Title } from '@tremor/react';
 
 export default function Home() {
   return (
     <div className='min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] text-6xl p-10 flex flex-col justify-center items-center'>
-      <Card className='bg-slate-200 py-4 px-6'>
-        <Text className='text-6-xl mb-10 text-center font-bold'>
-          Nimbus Weather AI
-        </Text>
-        <Subtitle className='text-center text-xl'>
+      <Card className='bg-slate-200 py-4 px-6 rounded-lg'>
+        <Title className='text-6-xl mb-6 text-center font-bold'>
+          Nimbus AI
+        </Title>
+        <Subtitle className='text-center text-xl text-slate-500 font-light'>
           Powered by OpenAI, Next.js, Tailwind CSS, Typescript, Tremor 2.0 +
           More
         </Subtitle>
 
         <Divider className='my-10' />
-        <Card className='bg-gradient-to-br from-[#394F68]/70 to-[#183B7E]/70'>
+        <Card className='bg-gradient-to-br from-[#394F68]/70 to-[#183B7E]/70 text-base font-semibold text-slate-800 p-3 rounded-lg shadow-md shadow-slate-800/70'>
           {/* City Picker  */}
           <CityPicker />
         </Card>
--- src/components/CityPicker.tsx
+++ src/components/CityPicker.tsx
@@ -1,21 +1,98 @@
 /* eslint-disable react/function-component-definition */
 'use client';
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Country, City, State } from 'country-state-city';
 import Select from 'react-select';
+import { useRouter } from 'next/navigation';
+import { BsGlobeAmericas } from 'react-icons/bs';
+import { GiModernCity } from 'react-icons/gi';
+
+type option = {
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+};
+
+type cityOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    CountryCode: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+};
 
 const options = Country.getAllCountries().map((country) => ({
-    value: {
-        latitude: country.latitude,
-        longitude: country.longitude,
-        isoCode: country.isoCode,
-    },
-    label: country.name
-}))
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    isoCode: country.isoCode,
+  },
+  label: country.name,
+}));
+
 function CityPicker() {
   const [selectedCountry, setSelectedCountry] = useState<option>(null);
-  return <div><Select options={options}/></div>;
-}
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
 
+  const handleSelectedCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  };
+  const handleSelectedCity = (option: cityOption) => {
+    setSelectedCity(option);
+    router.push(
+      `/location/${option?.value.latitude}/${option?.value.longitude}`,
+    );
+  };
+
+  return (
+    <div className='space-y-2'>
+      {/* Country Picker  */}
+      <div className='space-y-2'>
+        <div className='flex items-center justify-start space-x-2 py-2 text-slate-300'>
+          <BsGlobeAmericas className='h-5 w-5 text-slate-300' />
+          <label htmlFor='country'>Country</label>
+        </div>
+        <Select
+          value={selectedCountry}
+          onChange={handleSelectedCountry}
+          options={options}
+        />
+      </div>
+      {/* City Picker  */}
+      {selectedCountry && (
+        <div className='space-y-2'>
+          <div className='flex items-center justify-start space-x-2 py-2 text-slate-300'>
+            <GiModernCity className='h-5 w-5 text-slate-300' />
+            <label htmlFor='country'>City</label>
+          </div>
+          <Select
+            value={selectedCity}
+            onChange={handleSelectedCity}
+            options={City.getCitiesOfCountry(
+              selectedCountry.value.isoCode,
+            )?.map((city) => ({
+              value: {
+                latitude: city.latitude,
+                longitude: city.longitude,
+                countryCode: city.countryCode,
+                name: city.name,
+                stateCode: city.stateCode,
+              },
+              label: city.name,
+            }))}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
 export default CityPicker;
